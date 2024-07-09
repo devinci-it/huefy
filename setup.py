4c85c40b57c59b5084043f50be6c881ada3b8255
devinci-it <vince.dev@icloud.com>
2024-07-09 11:42:49 -0700

    CHORE:v1.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='huefy',
-    version='1.2.1',
+    version='1.2.2',
     packages=find_packages(where='src'),  # Find packages under 'src'
     package_dir={'': 'src'},              # Package root directory is 'src'
     package_data={
